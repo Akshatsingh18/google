63ceb935f9c3f59481f2b89112833026d41d1fe8
AKSHAT SINGH <81671650+Akshatsingh18@users.noreply.github.com>
2021-09-06 16:08:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+var sear = document.getElementById("link");
+var ele = document.getElementById("searchbox");
+sear.addEventListener("click",function()
+{
+    search();
+})
+
+function search()
+{
+    const input =  ele.Value;
+    alert(ele);
+    window.location.href = "https://www.youtube.com/";
+}
